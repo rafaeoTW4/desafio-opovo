4bcec15977e5050a5da341ee16d6b85dcd7e2cc8
Rafael Parente <164420722+rafaeoTW4@users.noreply.github.com>
2025-09-25 11:26:26 -0300

    Update api.php

--- backend/api.php
+++ backend/api.php
@@ -1,1 +1,12 @@
-php -S localhost:8000
+# Use 0.0.0.0 para permitir que outras máquinas na rede se conectem
+php -S 0.0.0.0:8000
+
+# Verifique o status do firewall
+sudo ufw status
+
+# Se estiver ativo, permita o tráfego na porta 8000
+sudo ufw allow 8000
+
+# Recarregue as regras
+sudo ufw reload
+
